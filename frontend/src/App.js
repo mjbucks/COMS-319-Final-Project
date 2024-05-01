e864dfd24c33bf456dfb7438506fe1c2f433aa94
Tate Myers <tatmyers@iastate.edu>
2024-05-01 13:01:37 -0500

    select screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
+import { ViewCharacters } from "./components/CharacterSelect";
 import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
     <div className="App">
       <LoginForm/>
+      <ViewCharacters/>
     </div>
   );
 }
