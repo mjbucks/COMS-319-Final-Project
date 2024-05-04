ac84fd3dd4b0a0118cdc8af02887d43dc583bfa2
Tate Myers <tatmyers@iastate.edu>
2024-05-04 14:56:07 -0500

    fix

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -4,7 +4,7 @@ import "../styles/style.css"
 
 import PropTypes from "prop-types";
 
-export function ViewGame({p1Character, p2Character, setScreen, p1username, p2username}) {
+export function ViewGame({ p1Character, p2Character, setScreen, p1username, p2username }) {
     const statBoostTracker = {
         hp: 0,
         attack: 1,
@@ -28,75 +28,75 @@ export function ViewGame({p1Character, p2Character, setScreen, p1username, p2use
     //get 2 for each player
 
     useEffect(() => {
-        setP1Stats(stats => ({...stats, hp: p1Character.stats.hp * 2.5}));
-        setP2Stats(stats => ({...stats, hp: p2Character.stats.hp * 2.5}));
+        setP1Stats(stats => ({ ...stats, hp: p1Character.stats.hp * 2.5 }));
+        setP2Stats(stats => ({ ...stats, hp: p2Character.stats.hp * 2.5 }));
     }, [p1Character, p2Character]);
 
 
     function handleGameCalcs() {
         //do stat increases here
-        
+
         let p1hits = p1move.effect.hits > 0 ? p1move.effect.hits : 1;
         let p2hits = p2move.effect.hits > 0 ? p2move.effect.hits : 1;
         let p1crit = ((Math.floor(Math.random() * 1000 / p1hits) <= p1Character.stats.luck) && p2Character.ability != "Point guard") ? 2.5 : 1;
         let p2crit = ((Math.floor(Math.random() * 1000 / p2hits) <= p2Character.stats.luck) && p1Character.ability != "Point guard") ? 2.5 : 1;
-        let p1atk = Math.floor((((52 * p1move.physical * ((p1Character.stats.attack * p1stats.attack)/ (p2Character.stats.defense * p2stats.defense)))/50) + 2)* p1crit * p1hits);
-        p1atk = Math.floor(p1atk + (((52 * p1move.special * ((p1Character.stats.special_attack * p1stats.special_attack)/ (p2Character.stats.special_defense * p2stats.special_defense)))/50) + 2)* p1crit * p1hits);//Crit rate at the end
-        let p2atk = Math.floor((((52 * p2move.physical * ((p2Character.stats.attack * p2stats.attack) / (p1Character.stats.defense * p1stats.defense)))/50) + 2)* p2crit) * p2hits;
-        p2atk = Math.floor(p2atk + (((52 * p2move.special * ((p2Character.stats.special_attack * p2stats.special_attack) / (p1Character.stats.special_defense * p1stats.special_defense)))/50) + 2)* p2crit * p2hits);
+        let p1atk = Math.floor((((52 * p1move.physical * ((p1Character.stats.attack * p1stats.attack) / (p2Character.stats.defense * p2stats.defense))) / 50) + 2) * p1crit * p1hits);
+        p1atk = Math.floor(p1atk + (((52 * p1move.special * ((p1Character.stats.special_attack * p1stats.special_attack) / (p2Character.stats.special_defense * p2stats.special_defense))) / 50) + 2) * p1crit * p1hits);//Crit rate at the end
+        let p2atk = Math.floor((((52 * p2move.physical * ((p2Character.stats.attack * p2stats.attack) / (p1Character.stats.defense * p1stats.defense))) / 50) + 2) * p2crit) * p2hits;
+        p2atk = Math.floor(p2atk + (((52 * p2move.special * ((p2Character.stats.special_attack * p2stats.special_attack) / (p1Character.stats.special_defense * p1stats.special_defense))) / 50) + 2) * p2crit * p2hits);
 
         //NOW CALC SPEED
         let p1turn = false;
-        if((p1Character.stats.speed * p1stats.speed) > (p2Character.stats.speed * p2stats.speed)){
+        if ((p1Character.stats.speed * p1stats.speed) > (p2Character.stats.speed * p2stats.speed)) {
             p1turn = true;
         }
-        else if((p1Character.stats.speed * p1stats.speed) == (p2Character.stats.speed * p2stats.speed)){
+        else if ((p1Character.stats.speed * p1stats.speed) == (p2Character.stats.speed * p2stats.speed)) {
             let random = Math.floor(Math.random() * 2)
-            if(random == 1){
+            if (random == 1) {
                 p1turn = true;
             }
-            else{
+            else {
                 p1turn = false;
             }
         }
-        else{
+        else {
             p1turn = false;
         }
-        if(gameStats.inverse){
-            if(p1turn){
+        if (gameStats.inverse) {
+            if (p1turn) {
                 p1turn = false;
             }
-            else{
+            else {
                 p1turn = true;
             }
         }
-        if((p1move.effect.priority ? p1move.effect.priority : 0) > (p2move.effect.priority ? p2move.effect.priority : 0)){
+        if ((p1move.effect.priority ? p1move.effect.priority : 0) > (p2move.effect.priority ? p2move.effect.priority : 0)) {
             p1turn = true;
         }
-        if((p1move.effect.priority ? p1move.effect.priority : 0) < (p2move.effect.priority ? p2move.effect.priority : 0)){
+        if ((p1move.effect.priority ? p1move.effect.priority : 0) < (p2move.effect.priority ? p2move.effect.priority : 0)) {
             p1turn = false;
         }
         //Ability check
-        if(p1Character.ability === ("Shiver")){
-            if(gameStats.snow){
+        if (p1Character.ability === ("Shiver")) {
+            if (gameStats.snow) {
                 p1atk = p1atk * 2;
             }
         }
-        if(p1Character.ability === ("LAYD")){
-            if(p2move.physical > 0){
+        if (p1Character.ability === ("LAYD")) {
+            if (p2move.physical > 0) {
                 p1stats.hp += p2atk * 0.1
             }
         }
-        if(p1Character.ability === ("Speed Run")){
-            if((Math.floor(Math.random() * 1000) < 299)){
+        if (p1Character.ability === ("Speed Run")) {
+            if ((Math.floor(Math.random() * 1000) < 299)) {
                 p1atk = p1atk * 1.3
             }
         }
-        if(p1Character.ability === ("Peanut Butter Paws")){
+        if (p1Character.ability === ("Peanut Butter Paws")) {
             p1stats.speed += p1stats.speed * (-0.5);
         }
-        if(p1Character.ability === ("Lightweight")){
-            if(p1move.name === ("Drawn Instincts")){
+        if (p1Character.ability === ("Lightweight")) {
+            if (p1move.name === ("Drawn Instincts")) {
                 p1stats.attack += p1stats.attack * (0.5);
                 p1stats.defense += p1stats.defense * (0.5);
                 p1stats.special_attack += p1stats.special_attack * (0.5);
@@ -104,31 +104,31 @@ export function ViewGame({p1Character, p2Character, setScreen, p1username, p2use
                 p1stats.speed += p1stats.speed * (0.5);
             }
         }
-        if(p1Character.ability === ("ACL tear")){
+        if (p1Character.ability === ("ACL tear")) {
             p1stats.speed += p1stats.speed * (-0.5);
             p1stats.attack += p1stats.attack * (0.5);
         }
         //P2//////////////////////////////////////////////////////////////////
-        if(p2Character.ability === ("Shiver")){
-            if(gameStats.snow){
+        if (p2Character.ability === ("Shiver")) {
+            if (gameStats.snow) {
                 p2atk = p2atk * 2;
             }
         }
-        if(p2Character.ability === ("LAYD")){
-            if(p1move.physical > 0){
+        if (p2Character.ability === ("LAYD")) {
+            if (p1move.physical > 0) {
                 p2stats.hp += p1atk * 0.1
             }
         }
-        if(p2Character.ability === ("Speed Run")){
-            if((Math.floor(Math.random() * 1000) < 299)){
+        if (p2Character.ability === ("Speed Run")) {
+            if ((Math.floor(Math.random() * 1000) < 299)) {
                 p2atk = p2atk * 1.3
             }
         }
-        if(p2Character.ability === ("Peanut Butter Paws")){
+        if (p2Character.ability === ("Peanut Butter Paws")) {
             p2stats.speed += p2stats.speed * (-0.5);
         }
-        if(p2Character.ability === ("Lightweight")){
-            if(p2move.name === ("Drawn Instincts")){
+        if (p2Character.ability === ("Lightweight")) {
+            if (p2move.name === ("Drawn Instincts")) {
                 p2stats.attack += p2stats.attack * (0.5);
                 p2stats.defense += p2stats.defense * (0.5);
                 p2stats.special_attack += p2stats.special_attack * (0.5);
@@ -136,20 +136,20 @@ export function ViewGame({p1Character, p2Character, setScreen, p1username, p2use
                 p2stats.speed += p2stats.speed * (0.5);
             }
         }
-        if(p2Character.ability === ("ACL tear")){
+        if (p2Character.ability === ("ACL tear")) {
             p2stats.speed += p2stats.speed * (-0.5);
             p2stats.attack += p2stats.attack * (0.5);
         }
         //now apply
-        if (p1turn){
+        if (p1turn) {
             p2stats.hp -= p1atk;
-            if(!p2stats.hp <= 0){
+            if (!p2stats.hp <= 0) {
                 p1stats.hp -= p2atk;
             }
         }
-        else{
+        else {
             p1stats.hp -= p2atk;
-            if(!p1stats.hp <= 0){
+            if (!p1stats.hp <= 0) {
                 p2stats.hp -= p1atk;
             }
         }
@@ -170,40 +170,40 @@ export function ViewGame({p1Character, p2Character, setScreen, p1username, p2use
             p2stats.special_defense += p2stats.special_defense * ((p2move.effect.spdef ? p2move.effect.spdef : 0) * 0.5);
             p2stats.speed += p2stats.speed * ((p2move.effect.spe ? p2move.effect.spe : 0) * 0.5);
         }
-            if(gameStats.snow != true && p1move.effect.snow){
-                gameStats.snow = true
-            }
-            if(gameStats.snow != true && p2move.effect.snow){
-                gameStats.snow = p2move.effect?.snow
-            }
-            if(gameStats.inverse != true && p1move.effect.inverse){
-                gameStats.inverse = p2move.effect?.inverse
-            }
-            if(gameStats.inverse != true && p1move.effect.snow){
-                gameStats.inverse = p2move.effect?.inverse
-            }
+        if (gameStats.snow != true && p1move.effect.snow) {
+            gameStats.snow = true
+        }
+        if (gameStats.snow != true && p2move.effect.snow) {
+            gameStats.snow = p2move.effect?.snow
+        }
+        if (gameStats.inverse != true && p1move.effect.inverse) {
+            gameStats.inverse = p2move.effect?.inverse
+        }
+        if (gameStats.inverse != true && p1move.effect.snow) {
+            gameStats.inverse = p2move.effect?.inverse
+        }
 
 
     }
-    function isGameOver(){
-        if (p1stats.hp < 0){
+    function isGameOver() {
+        if (p1stats.hp < 0) {
             //add to DB, pop up 
-            setWinner(p2username.username);
+            setWinner(p2username);
         }
-        else if (p2stats.hp < 0){
-            setWinner(p1username.username);
+        else if (p2stats.hp < 0) {
+            setWinner(p1username);
         }
     }
-    function handleSetP1Move(move){
+    function handleSetP1Move(move) {
         setP1Move(move);
         setP1MovesVisible(false);
     };
 
-    function handleSetP2Move(move){
+    function handleSetP2Move(move) {
         setP2Move(move);
         setP2MovesVisible(false);
     };
-    function handleNewTurn(){
+    function handleNewTurn() {
         handleGameCalcs()
         handleMoveEffects()
         setP1Move({})
@@ -211,45 +211,49 @@ export function ViewGame({p1Character, p2Character, setScreen, p1username, p2use
         isGameOver();
     };
 
+    function handleBackToHome() {
+        setScreen("home");
+    }
+
     return (
-    <div className="container">
-        {winner === '' ? (
-            <>
-                <p>{p1Character.name} {p2Character.name} {p1username} {p2username}</p>
-                <button onClick={() => setP1MovesVisible(!p1MovesVisible)} disabled={p1MovesVisible}>
-                    Toggle {p1Character.name}'s Moves
-                </button>
-                {p1MovesVisible && (
-                    <ul>
-                        <button onClick={() => handleSetP1Move(p1Character.moves.move1)}>{p1Character.moves.move1.name}</button>
-                        <button onClick={() => handleSetP1Move(p1Character.moves.move2)}>{p1Character.moves.move2.name}</button>
-                        <button onClick={() => handleSetP1Move(p1Character.moves.move3)}>{p1Character.moves.move3.name}</button>
-                    </ul>
-                )}
-                <button onClick={() => setP2MovesVisible(!p2MovesVisible)} disabled={p2MovesVisible}>
-                    Toggle {p2Character.name}'s Moves
-                </button>
-                {p2MovesVisible && (
-                    <ul>
-                        <button onClick={() => handleSetP2Move(p2Character.moves.move1)}>{p2Character.moves.move1.name}</button>
-                        <button onClick={() => handleSetP2Move(p2Character.moves.move2)}>{p2Character.moves.move2.name}</button>
-                        <button onClick={() => handleSetP2Move(p2Character.moves.move3)}>{p2Character.moves.move3.name}</button>
-                    </ul>
-                )}
-                <p>{p1move.name} {p2move.name}</p>
-                <button onClick={handleNewTurn} disabled={!p1move.name || !p2move.name}>
-                    Begin Turn?
-                </button>
-            </>
-        ) : (
-            <div>{winner}
-            <button
-            onClick={setScreen("home")}> Return Home
-                </button>
-            </div>
-        )}
-    </div>
-);
+        <div className="container">
+            {winner === '' ? (
+                <>
+                    <p>{p1Character.name} {p2Character.name} {p1username} {p2username}</p>
+                    <button onClick={() => setP1MovesVisible(!p1MovesVisible)} disabled={p1MovesVisible}>
+                        Toggle {p1Character.name}'s Moves
+                    </button>
+                    {p1MovesVisible && (
+                        <ul>
+                            <button onClick={() => handleSetP1Move(p1Character.moves.move1)}>{p1Character.moves.move1.name}</button>
+                            <button onClick={() => handleSetP1Move(p1Character.moves.move2)}>{p1Character.moves.move2.name}</button>
+                            <button onClick={() => handleSetP1Move(p1Character.moves.move3)}>{p1Character.moves.move3.name}</button>
+                        </ul>
+                    )}
+                    <button onClick={() => setP2MovesVisible(!p2MovesVisible)} disabled={p2MovesVisible}>
+                        Toggle {p2Character.name}'s Moves
+                    </button>
+                    {p2MovesVisible && (
+                        <ul>
+                            <button onClick={() => handleSetP2Move(p2Character.moves.move1)}>{p2Character.moves.move1.name}</button>
+                            <button onClick={() => handleSetP2Move(p2Character.moves.move2)}>{p2Character.moves.move2.name}</button>
+                            <button onClick={() => handleSetP2Move(p2Character.moves.move3)}>{p2Character.moves.move3.name}</button>
+                        </ul>
+                    )}
+                    <p>{p1move.name} {p2move.name}</p>
+                    <button onClick={handleNewTurn} disabled={!p1move.name || !p2move.name}>
+                        Begin Turn?
+                    </button>
+                </>
+            ) : (
+                <div>{winner}
+                    <button
+                        onClick={handleBackToHome}> Return Home
+                    </button>
+                </div>
+            )}
+        </div>
+    );
 
 }
 ViewGame.propTypes = {
@@ -258,6 +262,6 @@ ViewGame.propTypes = {
     setScreen: PropTypes.func,
     p1username: PropTypes.func,
     p2username: PropTypes.func,
-  };
-  
-  export default ViewGame;
+};
+
+export default ViewGame;
