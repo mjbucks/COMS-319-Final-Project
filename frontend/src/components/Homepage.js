08500f7be8e9d19faaa9b2ed659627a848aa5987
Tate Myers <tatmyers@iastate.edu>
2024-05-04 21:51:26 -0500

    done

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -10,6 +10,7 @@ export default function Homepage({
             <div className="user-greetings">
                 <div className="left-greeting">Hello {p1User.username} ({p1User.win}-{p1User.loss})</div>
                 <button className="play-button" onClick={() => setScreen('chooseCharacter')}>Play</button>
+                <button className="play-button" onClick={() => setScreen('authors')}>Authors</button>
                 <div className="right-greeting">Hello {p2User.username} ({p2User.win}-{p2User.loss})</div>
             </div>
             <div className="card">
@@ -18,7 +19,7 @@ export default function Homepage({
                 </div>
                 <div className="card-body">
                     <div className="description">
-                        <p>Battle Buddies is a web-based game where players can battle one another as their friends reimagined as powerful beings!
+                        <p>Friends Smackdown is a web-based game where players can battle one another as their friends reimagined as powerful beings!
                             Select your character that you want to play as and create a strategy to ensure your success! The wiki 
                             was created in anticipation of the full game's release, some of the stated statistics are subject to change.
                             These changes could include but are not limited to stat changes, move changes, or new characters.
@@ -36,11 +37,13 @@ export default function Homepage({
                 </div>
                 <div className="card-body">
                     <div className="description">
-                        <p>You play by not being gay... Thoughts?
+                        <p>Grab a friend and select a friend! Choose from your character's moves in order to position yourself for victory!
+                            Simply choose a move, pass the device to your opponent, they choose their move, and then end the turn to see the results. Whoever makes the opponents hitpoints drop below zero first loses!
+
                         </p>
                     </div>
                     <div className="image-container">
-                        <img alt="How to Play Image" />
+                        <img src="./Image/gameplay.png" alt="How to Play Image" />
                     </div>
                 </div>
             </div>
--- frontend/src/components/TitleText.js
+++ frontend/src/components/TitleText.js
@@ -5,7 +5,7 @@ export default function TitleText() {
     return (
         <div>
             <section className="header-main">
-                <h1 style={{ textAlign: "center", paddingTop: "1%" }}>Welcome to Battle Buddies </h1>
+                <h1 style={{ textAlign: "center", paddingTop: "1%" }}>Welcome to Friend Smackdown </h1>
                 <h3 style={{ textAlign: "center" }}>Battle versus your friends as your favorite characters!</h3>
             </section>
         </div>
